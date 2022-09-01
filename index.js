794cc9563b2deb51b5488c7aa7645f80fa62b9f4
Tess <tere0506@stud.kea.dk>
2022-09-01 10:55:39 +0200

    Exercise Done

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+let counter;
+
+/* // Count 0 to 9 //
+for (counter = 0; counter < 10; counter++) {
+  console.log("Count Increasing from 0: " + counter);
+} */
+
+/* // Count 1 to 10 //
+for (counter = 1; counter < 11; counter++) {
+  console.log("Count Increasing from 1: " + counter);
+} */
+
+/* // Count 10 to 0 //
+for (counter = 10; counter > 0; counter--) {
+  console.log("Count Decreasing from 10: " + counter);
+}
+console.log("liftoff"); */
+
+/* // Count 1 to 19 (odd) //
+for (counter = 1; counter < 20; counter = counter + 2) {
+  console.log("Counter Increasing by 2 (odd): " + counter);
+} */
+
+/* // Count 1 to 16777216 //
+for (counter = 1; counter < 16777217; counter = counter * 2) {
+  console.log("Counter Increasing by doubling: " + counter);
+} */
+
+/* // Count 11 to 138 (by 3) //
+for (counter = 111; counter < 139; counter = counter + 3) {
+  console.log("Counter Increasing in 3's: " + counter);
+} */
+
+/* // Count 100 to 0 (by 10) // 
+for (counter = 100; counter > -1; counter = counter - 10) {
+  console.log("Counter Decreasing by 10's: " + counter);
+} */
