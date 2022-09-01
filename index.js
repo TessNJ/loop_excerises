3e42f29530fa46e8bd57f8ab62b3dec09c89ebbf
Tess <tere0506@stud.kea.dk>
2022-09-01 11:29:21 +0200

    "done"

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ for (counter = 10; counter >= 0; counter--) {
 console.log("liftoff"); */
 
 /* // Count 1 to 19 (odd) //
-for (counter = 1; counter <= 19; counter = counter + 2) {
+for (counter = 1; counter <= 19; counter += 2) {
   console.log("Counter Increasing by 2 (odd): " + counter);
 } */
 
 /* // Count 1 to 16777216 //
-for (counter = 1; counter <= 16777216; counter = counter * 2) {
+for (counter = 1; counter <= 16777216; counter *= 2) {
   console.log("Counter Increasing by doubling: " + counter);
 } */
 
 /* // Count 11 to 138 (by 3) //
-for (counter = 111; counter <= 138; counter = counter + 3) {
+for (counter = 111; counter <= 138; counter += 3) {
   console.log("Counter Increasing in 3's: " + counter);
 } */
 
 /* // Count 100 to 0 (by 10) // 
-for (counter = 100; counter >= 0; counter = counter - 10) {
+for (counter = 100; counter >= 0; counter -= 10) {
   console.log("Counter Decreasing by 10's: " + counter);
 } */
